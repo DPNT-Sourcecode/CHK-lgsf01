90424cd5ddc1bf4ff8e890ea1f8fd0ee09c28bf3
sbx_user1051 <sbx_user1051@169.254.15.229>
2020-02-18 21:23:46 +0000

    Tue Feb 18 21:23:46 UTC 2020

--- src/main/java/befaster/solutions/CHK/Basket.java
+++ src/main/java/befaster/solutions/CHK/Basket.java
@@ -1,4 +1,25 @@
 package befaster.solutions.CHK;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Basket {
+
+  private PriceList priceList;
+
+  private Map<String, Integer> quantities = new HashMap<>();
+
+  public void addItem(String item) {
+
+  }
+
+
+
+  public void getTotal() {
+
+  }
+
+
+
 }
+
