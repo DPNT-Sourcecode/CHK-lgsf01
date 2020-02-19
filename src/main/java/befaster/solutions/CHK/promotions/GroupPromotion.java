d20947a91da9cac1f8a8dab874792b2e0cd18f98
sbx_user1051 <sbx_user1051@169.254.58.45>
2020-02-19 00:35:27 +0000

    Wed Feb 19 00:35:27 UTC 2020

---
+++ src/main/java/befaster/solutions/CHK/promotions/GroupPromotion.java
@@ -0,0 +1,12 @@
+package befaster.solutions.CHK.promotions;
+
+import befaster.solutions.CHK.Basket;
+
+public class GroupPromotion implements Promotion {
+
+
+  @Override
+  public void apply(Basket basket) {
+
+  }
+}
